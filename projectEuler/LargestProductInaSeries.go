5912fa5e5dcc4bb0dc52f0d1e6979ee1c5e57684
EmmanuelTobi <emmanuel.a.oluwatobi@gmail.com>
2020-01-08 22:04:34 +0100

    ProductinSeries

---
+++ projectEuler/LargestProductInaSeries.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "io/ioutil"
+    "os"
+)
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func main() {
+
+	f, err := os.Open("LargestProductInaSeries")
+    check(err)
+
+    b1 := make([]byte, 5)
+    n1, err := f.Read(b1)
+    check(err)
+    fmt.Printf("%d bytes: %s\n", n1, string(b1[:n1]))
+}
