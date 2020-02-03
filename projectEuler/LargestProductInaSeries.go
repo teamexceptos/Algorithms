e0b62c156de353b3700604f9c1b3b88f971bd31b
EmmanuelTobi <emmanuel.a.oluwatobi@gmail.com>
2020-02-03 09:19:34 +0100

    largest product in a series...solved and finished

--- projectEuler/LargestProductInaSeries.go
+++ projectEuler/LargestProductInaSeries.go
@@ -14,9 +14,20 @@ func check(e error) {
     }
 }
 
+func AdjacentProduct(index, total int64, digits []int64) int64 {
+
+    product := int64(1)
+    for i := int64(0); i < total; i++ {
+            product *= digits[index+i]
+    }
+
+    return product
+}
+
 func main() {
 
-	var algoDir = "/home/cosmic/Documents/allcodings/"
+    var algoDir = "/home/cosmic/Documents/allcodings/"
+    var digits []int64
 
     file, err := os.Open(algoDir + "/Algorithms/projectEuler/LargestProductInaSeries")
 
@@ -31,12 +42,38 @@ func main() {
     for scanner.Scan() {
 
         lineStr := scanner.Text()
-        // num, _ := strconv.Atoi(lineStr)
-        
-        fmt.Println(lineStr)
+        for _, runeValue := range lineStr {
+
+            i, err := strconv.ParseInt(string(runeValue), 10, 0)
+            if err != nil {
+                    panic(err)
+            }
+            digits = append(digits, i)
+        } 
     }
 
     if err := scanner.Err(); err != nil {
         log.Fatal(err)
     }
+
+    var max, index int64 = 0, 0
+    for i := int64(0); i < int64(len(digits)-13); i++ {
+        product := AdjacentProduct(i, 13, digits)
+        if product > max {
+            max = product
+            index = i
+        }
+    }
+
+    fmt.Print("Index at ")
+    fmt.Println(index)
+
+    for i := int64(0); i < 13; i++ {
+        fmt.Print(digits[index+i])
+        if i != 12 {
+            fmt.Print(" * ")
+        }
+    }
+    fmt.Print(" = ")
+    fmt.Println(max)
 }
